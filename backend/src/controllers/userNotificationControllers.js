f75fcad816f6a043244861639808c58ae9dd9ce0
Thibaut <thibaut.mosteau@lilo.org>
2023-12-14 11:04:45 +0100

    usernotifiactions controllers and models

---
+++ backend/src/controllers/userNotificationControllers.js
@@ -0,0 +1,52 @@
+const tables = require("../tables");
+
+const browse = async (req, res, next) => {
+  try {
+    const users = await tables.User_notification.readAll();
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const read = async (req, res, next) => {
+  try {
+    const user = await tables.User_notification.read(req.params.id);
+    if (user == null) {
+      res.sendStatus(404);
+    } else {
+      res.json(user);
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+const add = async (req, res, next) => {
+  const user = req.body;
+
+  try {
+    const insertId = await tables.User_notification.create(user);
+    res.status(201).json({ insertId });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const destroy = async (req, res, next) => {
+  const { notification_id: notificationId, user_id: usersId } = req.body;
+
+  try {
+    await tables.User_notification.delete({ notificationId, usersId });
+    res.sendStatus(200);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  browse,
+  read,
+  add,
+  destroy,
+};
