8a6a906757701bbadee73bd5a91c6e2aed16544b
Thibaut <thibaut.mosteau@lilo.org>
2023-12-18 15:47:26 +0100

    create connection

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,16 @@
-// import React
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import App
+
 import App from "./App";
 import "./styles/index.scss";
 
-// imports pages
 import Connection from "./pages/Connection";
-// import Home from "./pages/Home";
-// import Rules from "./pages/Rules";
-// import Profile from "./pages/Profile";
-// import Idea from "./pages/Idea";
+import Home from "./pages/Home";
+import Rules from "./pages/Rules";
+import Profile from "./pages/Profile";
+import Idea from "./pages/Idea";
 
-// Router
 const router = createBrowserRouter([
   {
     path: "/",
@@ -23,24 +20,24 @@ const router = createBrowserRouter([
         index: true,
         element: <Connection />,
       },
-      // {
-      //   path: "/home",
-      //   element: <Home />,
-      //   children: [
-      //     {
-      //       path: "/profile/:id",
-      //       element: <Profile />,
-      //     },
-      //     {
-      //       path: "/Idea",
-      //       element: <Idea />,
-      //     },
-      //   ],
-      // },
-      // {
-      //   path: "/rules",
-      //   element: <Rules />,
-      // },
+      {
+        path: "/home",
+        element: <Home />,
+        children: [
+          {
+            path: "/profile/:id",
+            element: <Profile />,
+          },
+          {
+            path: "/Idea",
+            element: <Idea />,
+          },
+        ],
+      },
+      {
+        path: "/rules",
+        element: <Rules />,
+      },
     ],
   },
 ]);
