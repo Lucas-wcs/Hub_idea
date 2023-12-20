8beb5b89d61b7422fde86f5a4b87b39beb60d215
CarolineG85 <caroline.gallix85@gmail.com>
2023-12-20 22:22:57 +0100

    finished final idea moderator's modal

--- frontend/src/components/ModalFinalDecision.jsx
+++ frontend/src/components/ModalFinalDecision.jsx
@@ -4,40 +4,40 @@
 
 function ModaleFinaleDecision() {
   return (
-    <div className="modale-final-decision">
-      <div className="idea-final-container">
+    <div className="modal-final-decision">
+      <div className="final-decision-container">
         <div className="close-button">
-          <button type="button">close</button>
+          <button type="button">Close</button>
         </div>
-        <div className="header">
-          <div className="title">
-            <h1>Proposition: Réaliser un repas de Noël</h1>
-          </div>
+        <div className="title-container">
+          <h1>Proposition: Repas de Noël</h1>
         </div>
-        <div className="tall-idea-img-container">
-          <div className="idea-img-container">
-            <img src="/images/repas_noel.webp" alt=" repas de Noël" />
-          </div>
+        <div className="idea-picture-container">
+          <img src="/images/repas_noel.webp" alt="repas de Noël" />
         </div>
-
-        <div className="progress-percentage-container">
+        <div className="progress-bar-container">
           <progress id="file" value="65" max="100" />
           <p>65%</p>
-          <div className="comment-finale-decision">
-            <p>Commentaire:</p>
-            <textarea
-              name="comment-moderator"
-              id="comment-moderator"
-              cols="30"
-              rows="10"
-            />
+        </div>
+        <div className="comment-container">
+          <div className="comment">
+            <p>Commentaire</p>
           </div>
-          <div className="moderator-decision-buttons">
-            {/* voir si submit, rajouter name? */}
-            <button type="submit">Refuser l'idée</button>
-            <br />
-            <button type="submit">Approuver définitivement l'idée</button>
-          </div>
+          <textarea
+            name="comment-text"
+            id="comment"
+            cols="30"
+            rows="10"
+            placeholder="Mettre un commentaire si vous souhaitez expliquer votre décision  ... "
+          />
+        </div>
+        <div className="idea-final-buttons">
+          <button className="reject" type="submit">
+            Rejeter définitivement l'idée
+          </button>
+          <button className="accept" type="submit">
+            Accepter définitivement l'idée
+          </button>
         </div>
       </div>
     </div>
