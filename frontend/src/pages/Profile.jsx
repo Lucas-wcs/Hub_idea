8257591724f1562941b8f8983892d21a3c4f748b
CarolineG85 <caroline.gallix85@gmail.com>
2023-12-26 14:18:00 +0100

    adding image and function to show or not password

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,47 +1,75 @@
-// voir pour la navbar
-import { Link, useNavigate } from "react-router-dom";
-import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
 import axios from "axios";
 import { ThemeContext } from "../context/ThemeContext";
 import { UserContext } from "../context/UserContext";
 
 function Profile() {
   // const users = useLoaderData();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const { theme } = useContext(ThemeContext);
-  const { user } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
+  const [setPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showNewPassword, setShowNewPassword] = useState(false);
+  const [showConfirmPassword, setShowsConfirmPassword] = useState(false);
+  const [isEyeOpen, setIsEyeOpen] = useState(false);
+  const [isEyeOpenNew, setIsEyeOpenNew] = useState(false);
+  const [isEyeOpenConfirm, setIsEyeOpenConfirm] = useState(false);
+  // const [checkPassword, setCheckPassword] = useState(false);
 
   const handlePut = async (e) => {
     e.preventDefault();
-    const imageProfil = user.image_profil;
-    const firstname = e.target.firstname.value;
-    const lastname = e.target.lastname.value;
-    const email = e.target.email.value;
-    const password = e.target.password.value;
-    const isAdmin = user.is_administrator;
-    const isModerator = user.is_moderator;
+
+    const userToUpdate = {
+      id: user.id,
+      image_profil: user?.image_profil || "par default",
+      firstname: e.target.firstname.value,
+      lastname: e.target.lastname.value,
+      email: e.target.email.value,
+      password: e.target.password.value,
+      is_administrator: user.is_administrator,
+      is_moderator: user.is_moderator,
+    };
 
     try {
       // eslint-disable-next-line no-unused-vars
       const res = await axios.put(
         `${import.meta.env.VITE_BACKEND}/api/users/${user.id}`,
-        {
-          imageProfil,
-          firstname,
-          lastname,
-          email,
-          password,
-          is_administrator: isAdmin,
-          is_moderator: isModerator,
-        }
+        userToUpdate
       );
-
-      navigate("/home");
+      setUser(userToUpdate);
+      // navigate("/home");
     } catch (error) {
       console.error(error);
     }
   };
+
+  const handleShowPassword = (passwordType) => {
+    if (passwordType === "current") {
+      setShowPassword(!showPassword);
+      setIsEyeOpen((current) => !current);
+    } else if (passwordType === "new") {
+      setShowNewPassword(!showNewPassword);
+      setIsEyeOpenNew((current) => !current);
+    } else if (passwordType === "confirm") {
+      setShowsConfirmPassword(!showConfirmPassword);
+      setIsEyeOpenConfirm((current) => !current);
+    }
+  };
+
+  //   const handleCheckPassword = (e) => {
+  //     const newPassword = e.target.password.value;
+  //     const newPasswordConfirmed = e.target.confirmPassword.value;
+  //     if (newPassword === newPasswordConfirmed) {
+  //       setCheckPassword(true);
+  //     } else {
+  //       setCheckPassword(false);
+  // console.log("erreur")    }
+  //   };
 
   return (
     <div>
@@ -111,24 +139,65 @@ function Profile() {
                 </label>
                 <input
                   className="profile-input-pass"
-                  type="password"
-                  placeholder="Mot de passe actuel"
+                  type={showPassword ? "text" : "password"}
+                  defaultValue="Mot de passe actuel"
                   name="password"
+                  onChange={(e) => setPassword(e.target.value)}
                 />
-
+                <button
+                  className="toggle-button"
+                  type="button"
+                  onClick={() => handleShowPassword("current")}
+                >
+                  {isEyeOpen ? (
+                    <img src="/images/oeil-ouvert.png" alt="oeil ouvert" />
+                  ) : (
+                    <img src="/images/oeil-fermé.png" alt="oeil fermé" />
+                  )}
+                </button>
                 <input
                   className="profile-input-pass"
-                  type="password"
+                  type={showNewPassword ? "text" : "password"}
                   placeholder="Nouveau mot de passe"
                   name="password"
+                  value={newPassword}
+                  minLength={6}
+                  required
+                  onChange={(e) => setNewPassword(e.target.value)}
                 />
-
+                <button
+                  className="toggle-button"
+                  type="button"
+                  onClick={() => handleShowPassword("new")}
+                >
+                  {isEyeOpenNew ? (
+                    <img src="/images/oeil-ouvert.png" alt="oeil ouvert" />
+                  ) : (
+                    <img src="/images/oeil-fermé.png" alt="oeil fermé" />
+                  )}
+                </button>
                 <input
                   className="profile-input-pass"
-                  type="password"
+                  type={showConfirmPassword ? "text" : "password"}
+                  value={confirmPassword}
                   placeholder="Confirmation du nouveau mot de passe"
-                  name="check password"
+                  name="confirmPassword"
+                  minLength={6}
+                  required
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  // onChange={handleCheckPassword}
                 />
+                <button
+                  className="toggle-button"
+                  type="button"
+                  onClick={() => handleShowPassword("confirm")}
+                >
+                  {isEyeOpenConfirm ? (
+                    <img src="/images/oeil-ouvert.png" alt="oeil ouvert" />
+                  ) : (
+                    <img src="/images/oeil-fermé.png" alt="oeil fermé" />
+                  )}
+                </button>
               </div>
               <div className="submit-button">
                 <button type="submit">Sauvegarder</button>
